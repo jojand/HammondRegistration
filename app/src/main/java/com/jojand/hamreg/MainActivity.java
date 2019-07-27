27fbdeb6f19cd6103328c226783513d9d8ddad5c
Josef Janda <josef.janda@gmail.com>
2019-07-27 11:23:33 +0200

    WIP

--- app/src/main/java/com/jojand/hamreg/MainActivity.java
+++ app/src/main/java/com/jojand/hamreg/MainActivity.java
@@ -54,31 +54,31 @@ public class MainActivity extends AppCompatActivity {
     }
     
     private void calculateRegisters() {
-        LinearLayout firstRow = findViewById(R.id.linearLayout_firstRow);
-        LinearLayout secondRow = findViewById(R.id.linearLayout_secondRow);
-        LinearLayout resultRow = findViewById(R.id.linearLayout_resultRow);
-
-        for (int i=0; i<REGISTERS; i++) {
-            EditText inA = (EditText) firstRow.getChildAt(i);
-            EditText inB = (EditText) secondRow.getChildAt(i);
-            TextView resView = (TextView) resultRow.getChildAt(i);
-
-            String textA = inA.getText().toString();
-            String textB = inB.getText().toString();
-
-            double a=0, b=0;
-
-            try {
-                a = Double.parseDouble(textA);
-                b = Double.parseDouble(textB);
-            } catch (Exception e) {
-                Log.w(TAG, "some of the infput is invalid, using default value: 0");
-            }
-
-            double res = LogSum.logSum(a, b);
-
-            Log.d(TAG, String.format("calculateRegisters %d: result: %f", i, res));
-            resView.setText(String.format("%d", (int)res));
-        }
+//        LinearLayout firstRow = findViewById(R.id.linearLayout_firstRow);
+//        LinearLayout secondRow = findViewById(R.id.linearLayout_secondRow);
+//        LinearLayout resultRow = findViewById(R.id.linearLayout_resultRow);
+//
+//        for (int i=0; i<REGISTERS; i++) {
+//            EditText inA = (EditText) firstRow.getChildAt(i);
+//            EditText inB = (EditText) secondRow.getChildAt(i);
+//            TextView resView = (TextView) resultRow.getChildAt(i);
+//
+//            String textA = inA.getText().toString();
+//            String textB = inB.getText().toString();
+//
+//            double a=0, b=0;
+//
+//            try {
+//                a = Double.parseDouble(textA);
+//                b = Double.parseDouble(textB);
+//            } catch (Exception e) {
+//                Log.w(TAG, "some of the infput is invalid, using default value: 0");
+//            }
+//
+//            double res = LogSum.logSum(a, b);
+//
+//            Log.d(TAG, String.format("calculateRegisters %d: result: %f", i, res));
+//            resView.setText(String.format("%d", (int)res));
+//        }
     }
 }
